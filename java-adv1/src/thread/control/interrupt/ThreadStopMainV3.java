cb32cde63477a5f5316809019370f3ddb7893277
Roco-Lee <aowlrtmd@gmail.com>
2024-08-16 21:15:43 +0900

    240816 인터럽트

--- java-adv1/src/thread/control/interrupt/ThreadStopMainV3.java
+++ java-adv1/src/thread/control/interrupt/ThreadStopMainV3.java
@@ -3,13 +3,13 @@ package thread.control.interrupt;
 import static util.MyLogger.log;
 import static util.ThreadUtils.sleep;
 
-public class ThreadStopMainV2 {
+public class ThreadStopMainV3 {
     public static void main(String[] args) {
         MyTask task = new MyTask();
         Thread thread = new Thread(task, "work");
         thread.start();
 
-        sleep(4000);
+        sleep(100);
         log("작업 중단 지시 thread.interrupt()");
         thread.interrupt();
         log("work 스레드 인터럽트 상태1 = " + thread.isInterrupted());
@@ -18,18 +18,20 @@ public class ThreadStopMainV2 {
     static class MyTask implements Runnable {
         @Override
         public void run() {
-            try {
-                while (true) {
-                    log("작업 중");
-                    Thread.sleep(3000);
-                }
-            } catch (InterruptedException e) {
-                log("work 스레드 인터럽트 상태2 = " + Thread.currentThread().isInterrupted());
-                log("interrupt message = " + e.getMessage());
-                log("state = " + Thread.currentThread().getState());
-                log("자원 정리");
-                log("자원 종료");
+            while (!Thread.currentThread().isInterrupted()) { // 인터럽트 상태 변경 X
+                log("작업 중");
             }
+
+            log("work 스레드 인터럽트 상태2 = " + Thread.currentThread().isInterrupted());
+            try {
+                log("자원 정리");
+                Thread.sleep(1000);
+                log("자원 종료");
+            } catch (InterruptedException e) {
+                log("자원 정리 실패 - 자원 정리 중 인터럽트 발생");
+                log("work 스레드 인터럽트 상태3 = " + Thread.currentThread().isInterrupted());
+            }
+            log("작업 종료");
         }
     }
 }
